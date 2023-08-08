f43da7a53ef590944d8fdb10550d3d4ce2c18fde
Titus Wormer <tituswormer@gmail.com>
2023-08-08 15:02:34 +0200

    Refactor code-style

--- test/index.js
+++ test/index.js
@@ -1,80 +1,85 @@
 import assert from 'node:assert/strict'
-import {test} from 'node:test'
+import test from 'node:test'
 import {fromHtmlIsomorphic} from 'hast-util-from-html-isomorphic'
 
-test('parse document', () => {
-  const html = '<html><head></head><body></body></html>'
-  const tree = fromHtmlIsomorphic(html)
+test('hast-util-from-html-isomorphic', async function (t) {
+  await t.test('should parse a document', async function () {
+    const html = '<html><head></head><body></body></html>'
+    const tree = fromHtmlIsomorphic(html)
 
-  assert.deepEqual(tree, {
-    type: 'root',
-    children: [
-      {
-        type: 'element',
-        tagName: 'html',
-        properties: {},
-        children: [
-          {
-            type: 'element',
-            tagName: 'head',
-            properties: {},
-            children: []
-          },
-          {
-            type: 'element',
-            tagName: 'body',
-            properties: {},
-            children: []
-          }
-        ]
-      }
-    ]
+    assert.deepEqual(tree, {
+      type: 'root',
+      children: [
+        {
+          type: 'element',
+          tagName: 'html',
+          properties: {},
+          children: [
+            {
+              type: 'element',
+              tagName: 'head',
+              properties: {},
+              children: []
+            },
+            {
+              type: 'element',
+              tagName: 'body',
+              properties: {},
+              children: []
+            }
+          ]
+        }
+      ]
+    })
   })
-})
 
-test('parse single element fragment', () => {
-  const html = '<div><p></p></div>'
-  const tree = fromHtmlIsomorphic(html, {fragment: true})
+  await t.test('should support `fragment: true`', async function () {
+    const html = '<div><p></p></div>'
+    const tree = fromHtmlIsomorphic(html, {fragment: true})
 
-  assert.deepEqual(tree, {
-    type: 'root',
-    children: [
-      {
-        type: 'element',
-        tagName: 'div',
-        properties: {},
+    assert.deepEqual(tree, {
+      type: 'root',
+      children: [
+        {
+          type: 'element',
+          tagName: 'div',
+          properties: {},
+          children: [
+            {
+              type: 'element',
+              tagName: 'p',
+              properties: {},
+              children: []
+            }
+          ]
+        }
+      ]
+    })
+  })
+
+  await t.test(
+    'should support `fragment: true` w/ multiple children',
+    async function () {
+      const html = '<p></p><div></div>'
+      const tree = fromHtmlIsomorphic(html, {fragment: true})
+
+      assert.deepEqual(tree, {
+        type: 'root',
         children: [
           {
             type: 'element',
             tagName: 'p',
+            children: [],
+            properties: {}
+          },
+          {
+            type: 'element',
+            tagName: 'div',
             properties: {},
             children: []
           }
         ]
-      }
-    ]
-  })
-})
-
-test('parse multi element fragment', () => {
-  const html = '<p></p><div></div>'
-  const tree = fromHtmlIsomorphic(html, {fragment: true})
-
-  assert.deepEqual(tree, {
-    type: 'root',
-    children: [
-      {
-        type: 'element',
-        tagName: 'p',
-        children: [],
-        properties: {}
-      },
-      {
-        type: 'element',
-        tagName: 'div',
-        properties: {},
-        children: []
-      }
-    ]
-  })
+      })
+    }
+  )
 })
